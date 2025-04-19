46cd5227e2ba0d87c9fc334a23269c1fda731e66
Lin Qiumeng <linqiumeng22@gmail.com>
2025-04-19 12:11:35 -0700

    Add files via upload
    This is my first GitHub project.

---
+++ front-end.js
@@ -0,0 +1,40 @@
+waitlist = [];
+
+showlist();
+
+function showlist(){
+    let html = ``;
+    for (let index = 0; index < waitlist.length; index++) {
+        const block = waitlist[index];
+        const todo = block.item;
+        const duetime = block.dueday;
+        const show = `
+        <p class="css-item">${todo} ${duetime}
+        <button class="css-delete"
+        onclick="
+        waitlist.splice(${index},1)
+        showlist();
+        "
+        >Delete</button>
+        </p>
+        `
+        html += show;
+    }
+    document.querySelector('.js-show').innerHTML = html;
+}
+
+function adding(){
+    const event = document.querySelector('.js-input');
+    const eventvalue = event.value;
+
+    const due = document.querySelector('.js-date')
+    const duevalue = due.value;
+
+    waitlist.push({
+        item: eventvalue,
+        dueday: duevalue
+    });
+    console.log(waitlist);
+    showlist();
+}
+
